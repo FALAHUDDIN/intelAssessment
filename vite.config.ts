6096c3c72deded310408d456c8f30c219945434b
Falahuddin Abu Seman <falahuddinas@gmail.com>
2023-10-25 21:02:58 +0800

    deploy file added

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/vite-deploy/",
+    base: "/intelAssessment/",
     test: {
         globals: true,
         environment: "jsdom",
